7a5e53d0bd0a4ef3852bb3db49470bc7fa3b87d2
Priyanka <mandirakumari1538@gmail.com>
2023-04-03 22:47:59 +0530

    functions for different operations

--- Functioncreate.cpp
+++ Functioncreate.cpp
@@ -3,6 +3,19 @@
 #include <limits> // for numeric_limits
 
 using namespace std;
+float add(float num1, float num2){// function for addition
+    return num1+num2;
+}
+float sub(float num1,float num2){//function for subtraction
+    return num1-num2;
+}
+float mult(float num1,float num2)//function for multiplication
+{
+    return num1*num2;
+}
+float div(float num1,float num2){//function for division
+    return num1/num2;
+}
 
 int main() {
     // Declare variables
@@ -22,20 +35,20 @@ int main() {
     // Perform calculation
     switch(op) {
         case '+':
-            result = num1 + num2;
+            result = add(num1,num2);
             break;
         case '-':
-            result = num1 - num2;
+            result = sub(num1,num2);
             break;
         case '*':
-            result = num1 * num2;
+           result = mult(num1,num2);
             break;
         case '/':
             if (num2 == 0) {
                 cerr << "Error: division by zero\n";
                 return 1;
             }
-            result = num1 / num2;
+          result = div(num1,num2);
             break;
         default:
             cerr << "Error: invalid operator\n";
